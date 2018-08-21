e895a7f5f1a83dc11882ba731cd29817b56101c1
Leonardo Souza <leogsouza@gmail.com>
2018-08-20 21:13:52 -0300

    Handling HTTP POST Requests

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
+
+app.use(express.json());
 
 const genres = [
   {id: 1, name: 'Action'},
@@ -18,6 +21,31 @@ app.get('/api/genres/:id', (req, res) => {
 
   res.send(genre)
 });
+
+app.post('/api/genres', (req, res) => {
+
+  const {error} = validateGenre(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+
+  const genre = {
+    id: genres.length + 1,
+    name: req.body.name
+  }
+
+  genres.push(genre);
+  res.send(genre);
+})
+
+function validateGenre(genre) {
+  const schema = {
+    name: Joi.string().min(3).required()
+  }
+
+  return Joi.validate(genre, schema);
+
+}
 
 const port = process.env.PORT || 3000;
 app.listen(3000, () => console.log(`Listening port ${port}`));
