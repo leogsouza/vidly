55768024079f94c927a49d377a1ba88dd30ff203
Leonardo Souza <leogsouza@gmail.com>
2018-08-26 00:31:28 -0300

    Refactor

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Joi = require('joi');
+Joi.objectId =require('joi-objectid')(Joi);
 const express = require('express');
 const app = express();
 const customers = require('./routes/customers')
