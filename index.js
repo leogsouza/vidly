5d15cd9d92b4d00fe1f93084b046eae4d84b3dd8
Leonardo Souza <leogsouza@gmail.com>
2018-08-25 21:07:51 -0300

    Integrate API with Database

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+const mongoose = require('mongoose');
 const Joi = require('joi');
 const express = require('express');
 const app = express();
 const genres = require('./routes/genres')
+
+mongoose.connect('mongodb://localhost/vidly', {useNewUrlParser: true})
+.then(() => console.log('Connected to MongDB'))
+.catch(err => console.log('Could not connect to MongoDB', err));
 
 app.use(express.json());
 
