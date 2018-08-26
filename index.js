3a583a924f09baffbb879978fd8a8b0e9e6279e1
Leonardo Souza <leogsouza@gmail.com>
2018-08-26 17:57:17 -0300

    Add user api

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const customers = require('./routes/customers')
 const genres = require('./routes/genres')
 const movies = require('./routes/movies')
 const rentals = require('./routes/rentals')
+const users = require('./routes/users')
 
 mongoose.connect('mongodb://localhost/vidly', {useNewUrlParser: true})
 .then(() => console.log('Connected to MongDB'))
@@ -18,6 +19,7 @@ app.use('/api/customers', customers);
 app.use('/api/genres', genres);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
+app.use('/api/users', users);
 
 const port = process.env.PORT || 3000;
 app.listen(3000, () => console.log(`Listening port ${port}`));
