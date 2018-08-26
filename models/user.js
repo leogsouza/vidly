99e0aa7badce1015879dbc7c29af286f343c9c75
Leonardo Souza <leogsouza@gmail.com>
2018-08-26 19:31:57 -0300

    Hash password

--- models/user.js
+++ models/user.js
@@ -4,24 +4,30 @@ const Joi = require('joi');
 const User = mongoose.model('User', new mongoose.Schema({
   name: { 
     type:String,
+    minlength: 5,
+    maxlength: 50,
     required: true
   },
   email: {
     type: String,
     unique: true,
+    minlength: 5,
+    maxlength: 255,
     required: true
   },
   password: {
     type: String,
+    minlength: 5,
+    maxlength: 255,
     required: true
   }
 }));
 
 function validateUser(user) {
   const schema = {
-    name: Joi.string().min(3).required(),
-    email: Joi.string().email().required(),
-    password: Joi.string().required()
+    name: Joi.string().min(5).max(50).required(),
+    email: Joi.string().min(5).max(255).email().required(),
+    password: Joi.string().min(5).max(255).required()
   }
 
   return Joi.validate(user, schema);
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 const { User, validate } = require('../models/user');
 const express = require('express');
+const bcrypt = require('bcrypt');
+const _ = require('lodash');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  const users = await User.find().sort('name');
+  const users = await User.find().sort('name').select('_id name email');
   res.send(users);
 });
 
@@ -11,13 +13,16 @@ router.post('/', async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  const user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
-  });
+  let user = await User.findOne({ email: req.body.email });
+
+  if (user) return res.status(400).send('User already registered');
+
+  user = new User(_.pick(req.body, ['name', 'email', 'password']));
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash('1234', salt);
   await user.save();
-  res.send(user);
+
+  res.send(_.pick(user, ['_id', 'name', 'email']));
 });
 
 router.put('/:id', async (req, res) => {
@@ -44,7 +49,7 @@ router.delete('/:id', async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
-  const user = await User.findById(req.params.id);
+  const user = await User.findById(req.params.id).select('_id, name, email');
 
   if (!user) return res.status(404).send('The user with the given ID was not found');
 
