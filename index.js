906beda49f08836eb4c9084cdac8b0a9bad199eb
Leonardo Souza <leogsouza@gmail.com>
2018-08-25 23:06:35 -0300

    Api Movies

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Joi = require('joi');
 const express = require('express');
 const app = express();
 const genres = require('./routes/genres')
+const movies = require('./routes/movies')
 
 mongoose.connect('mongodb://localhost/vidly', {useNewUrlParser: true})
 .then(() => console.log('Connected to MongDB'))
@@ -11,6 +12,7 @@ mongoose.connect('mongodb://localhost/vidly', {useNewUrlParser: true})
 app.use(express.json());
 
 app.use('/api/genres', genres);
+app.use('/api/movies', movies);
 
 const port = process.env.PORT || 3000;
 app.listen(3000, () => console.log(`Listening port ${port}`));
--- routes/genres.js
+++ routes/genres.js
@@ -1,19 +1,6 @@
-const mongoose = require('mongoose');
-const Joi = require('joi');
+const {Genre, validate} = require('../models/genre');
 const express = require('express');
 const router = express.Router();
-
-const genreSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    minlength: 5,
-    maxlength: 50
-  }
-});
-
-const Genre = mongoose.model('Genre', genreSchema);
-
 
 router.get('/', async (req, res) => {
   const genres = await Genre.find().sort('name');
@@ -31,7 +18,7 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
 
-  const {error} = validateGenre(req.body);
+  const {error} = validate(req.body);
   if (error) {
     return res.status(400).send(error.details[0].message);
   }
@@ -46,7 +33,7 @@ router.post('/', async (req, res) => {
 });
 
 router.put('/:id', async (req, res) => {
-  const {error} = validateGenre(req.body);
+  const {error} = validate(req.body);
   if (error) {
     return res.status(400).send(error.details[0].message);
   }
@@ -69,13 +56,6 @@ router.delete('/:id', async (req, res) => {
   res.send(genre);
 })
 
-function validateGenre(genre) {
-  const schema = {
-    name: Joi.string().min(3).required()
-  }
 
-  return Joi.validate(genre, schema);
-
-}
 
 module.exports = router;
